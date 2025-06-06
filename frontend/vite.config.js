ebf2c71cae9338e23da571ce1ebd1ad2c04e47e0
michjt19 <81060930+michjt19@users.noreply.github.com>
2025-06-06 10:31:59 +0000

    t

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   root: '.',         // 👈 ensure Vite looks in the current directory
   plugins: [react()],
   server: {
+    host: 0.0.0.0,
     port: 5173,
     host: true,
     open: false       // ✅ prevents xdg-open error
